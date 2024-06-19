75a9b0cb28b312d839f7a3d8f30ce8b90fdbe910
Nkhaoula <khaoula.noumi.fr@gmail.com>
2024-06-19 10:00:00 +0200

    création de la «exercice2»

---
+++ js/tva.js
@@ -0,0 +1,14 @@
+//alert("TVA");
+
+let prix;
+let resultat;
+
+prix = prompt("Entrez votre prix hors tva : ");
+resultat = prix * 1.2
+console.log(resultat);
+
+
+
+
+
+
---
+++ js/valeur.js
@@ -0,0 +1,38 @@
+//alert("valeur");
+
+let a, b, c, d, e, g, resultat;
+
+a = 2;
+b = 8;
+b+= 2;
+c = a + b * b;
+d = a * b + b;
+e = a * (b + b);
+f = a * b / a;
+g = b / a * a
+
+resultat = a - 1;
+console.log(resultat);
+
+
+resultat = a++
+console.log(resultat);
+
+resultat = b
+console.log(resultat);
+
+resultat = c
+console.log(resultat);
+
+resultat = d
+console.log(resultat);
+
+resultat = e
+console.log(resultat);
+
+resultat = f
+console.log(resultat);
+
+resultat = g
+console.log(resultat);
+
