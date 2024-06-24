48e25eb7aa7ac5a69c7910fb6c7c7c1de07b16c9
Nkhaoula <khaoula.noumi.fr@gmail.com>
2024-06-24 14:33:49 +0200

    jour

---
+++ js/comparaison.js
@@ -0,0 +1,11 @@
+let nb1 = prompt('1er saisie');
+let nb2 = prompt('2eme saisie');
+
+
+if(nb1 === nb2){
+    alert('nb1 et nb2 sont égal');
+}else if (nb1 > nb2){
+    alert('nb1 plus grand que nb2');
+}else if (nb1 < nb2){
+    alert('nb1 plus petit que nb2');
+}
